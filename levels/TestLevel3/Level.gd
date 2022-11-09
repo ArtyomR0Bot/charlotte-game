76d656359135cabf90f04eeffe19ad6452c0fd85
Artyom M <amolodchikov@gmail.com>
2022-11-10 00:27:25 +0500

    unused variable

--- levels/TestLevel3/Level.gd
+++ levels/TestLevel3/Level.gd
@@ -4,7 +4,7 @@ extends Node2D
 onready var start_position = $Charlie.position
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if $Charlie.position.y > 500:
 		$Charlie.position = start_position
 		$Charlie.reset(true, $Charlie.IDLE)
